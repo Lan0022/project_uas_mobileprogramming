b10acdca52ac03813b45d6c45ad71dc5d3390b96
Lan0022 <afdalart2@gmail.com>
2024-12-26 23:05:50 +0700

    Membuat halaman dengan nama laucher.dart dari hasil memsihakan class di main.dart

---
+++ lib/launcher.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class LaucherPage extends StatefulWidget {
+  @override
+  State<LaucherPage> createState() => _LaucherPageState();
+}
+
+class _LaucherPageState extends State<LaucherPage> {
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the LaucherPage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text('ClothsLooks'),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: const Text(
+          'Launcher Page',
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
