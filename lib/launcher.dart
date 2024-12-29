dedb239e49aa003cf2e7ce5de9a9d5bbb93fe384
Lan0022 <afdalart2@gmail.com>
2024-12-29 23:36:12 +0700

    membuat sebuah fungsi untuk menampilkan logo setelah beberapa saat membukaa plikasi(Launcer)

--- lib/launcher.dart
+++ lib/launcher.dart
@@ -1,4 +1,7 @@
+import 'package:clothslooks/users/landingpage.dart' as users;
 import 'package:flutter/material.dart';
+import 'package:clothslooks/constans.dart';
+import 'dart:async';
 
 class LaucherPage extends StatefulWidget {
   @override
@@ -6,6 +9,28 @@ class LaucherPage extends StatefulWidget {
 }
 
 class _LaucherPageState extends State<LaucherPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startLaunching();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+  }
+
+  startLaunching() async {
+    var duration = const Duration(seconds: 3);
+    return new Timer(duration, () {
+      Navigator.of(context).pushReplacement(new MaterialPageRoute(builder: (_) {
+        return new users.LandingPage();
+      }));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -15,20 +40,35 @@ class _LaucherPageState extends State<LaucherPage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the LaucherPage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text('ClothsLooks'),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: const Text(
-          'Launcher Page',
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.circular(0)),
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                color: Colors.grey.shade200,
+                spreadRadius: 2,
+                offset: Offset(2, 4),
+                blurRadius: 5,
+              )
+            ],
+            gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Palette.bg1, Palette.bg2])),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Center(
+              child: new Image.asset(
+                "assets/logo2.png",
+                height: 90.0,
+                width: 270.0,
+              ),
+            ),
+          ],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
